68f0fd831f9a3cb4cc540deb7005451ca8449c06
Sevy Harris <harris.se@northeastern.edu>
2021-10-26 12:01:20 -0400

    update commit so example is even with more recent pushes to RMG-Py main

--- push_script_changes.sh
+++ push_script_changes.sh
@@ -3,6 +3,6 @@
 git branch -D rmgpy-rmg_tests_main_copy
 git branch -c rmgpy-rmg_tests_main_copy
 git checkout rmgpy-rmg_tests_main_copy
-git commit --allow-empty -m "rmgpy-7e4849b7890b4b7d5e6ba03f8535a48b1d1f0798"
+git commit --allow-empty -m "rmgpy-bcd469e65e09bcc94faa82aaa2ec002581e6fe5b"
 git push -uf origin rmgpy-rmg_tests_main_copy
 
